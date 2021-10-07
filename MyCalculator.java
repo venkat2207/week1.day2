7e0622f0e65747a1b7dfbff9b14b33a7a43ce0c8
venkat2207 <91820407+venkat2207@users.noreply.github.com>
2021-10-07 10:31:00 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,27 @@
+package week1.day2.Assignments1;
+
+import week1.day2.MyMobile;
+
+public class MyCalculator {
+	
+	public static void main(String[] args) {
+		
+		Calculator obj1=new Calculator();
+		
+		obj1.add(30, 40, 50);
+		
+		obj1.sub(23,34);
+		
+		obj1.mul(77,89);
+		
+		obj1.div(567,78);
+		
+	}
+	
+  
+		
+	
+
+	}
+
+
